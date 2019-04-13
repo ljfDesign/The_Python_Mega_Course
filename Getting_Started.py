5c7812fc7471474f69efe081bc32661d7d822569
ljfdesign <larry.frenzel.1990@gmail.com>
2019-04-13 14:42:04 +0430

    hello

---
+++ Getting_Started.py
@@ -0,0 +1,35 @@
+# The Command Line
+# - covered material -
+# *see folder content
+# *see current folder path
+# *create new folder
+# *create new file
+# *delete file
+# *go to another folder
+# *delete folder
+# *copy paste file
+# *move file
+# *rename file
+
+# LS lists files in directory
+# pwd - prints working directory
+# mkdir - creates new directory
+# CD folder/ - changes directory
+# touch file.txt - creates files
+# rm file.txt - removes files
+# cp ../file.txt - copies files
+# mv myprogram/ (to) folder/ - moves files
+# rm -r folder/ removed all files in directory
+# mv README.TXT readme.txt - renames files
+
+#---variables practice---
+character = "John Snow"
+#print(character)
+# bad examples of variables
+# print = 10
+# int = 121
+
+# good examples
+# printing = 10
+# print_variable = 10
+# int_var = 121
